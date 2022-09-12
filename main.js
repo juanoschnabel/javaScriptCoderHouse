58c08d55160d7d35eefc0a6f4ca4718f8268fab4
juanoschnabel <juanmschnabel@gmail.com>
2022-09-12 10:28:46 -0300

    Primera entrega del Proyecto Final

--- main.js
+++ main.js
@@ -1,77 +1,56 @@
-// TIPOS DE VEHÍCULOS
+//VEHICULOS
 const vehicles = [];
 vehicles.push({ id: "moto", price: 1000 });
 vehicles.push({ id: "auto", price: 1500 });
 vehicles.push({ id: "camioneta", price: 2000 });
-function final(vehicle, days) {
-  if (vehicle == 1000) {
-    alert(
-      `Usted seleccionó una moto para alquilar por el plazo de ` +
-        days +
-        " días!"
+vehicles.push({ id: "cuatriciclo", price: 3000 });
+let vehicle;
+let finalId;
+let finalPrice;
+function electionVehicles() {
+  let electionVehicle;
+  const mapVehicles = vehicles.map((vehicle) => vehicle.id);
+  do {
+    electionVehicle = prompt(
+      `Hola!! ¿Qué tipo de vehículo deseas alquilar?\n${mapVehicles.join("\n")}`
+    ).toLowerCase();
+    vehicle = vehicles.find((vehicle) => vehicle.id === electionVehicle);
+    electionVehicle = vehicles.some(
+      (vehicle) => vehicle.id === electionVehicle
     );
-  } else if (vehicle == 2000) {
-    alert(
-      `Usted seleccionó una camioneta para alquilar por el plazo de ` +
-        days +
-        " días!"
-    );
-  } else {
-    alert(
-      `Usted seleccionó un auto para alquilar por el plazo de ` +
-        days +
-        " días!"
-    );
-  }
-  if (vehicle === 1000) {
-    vehicle = 1000;
-    alert("El precio final del alquiler solicitado es $" + vehicle * days);
-  } else if (vehicle === 1500) {
-    vehicle = 1500;
-    alert("El precio final del alquiler solicitado es $" + vehicle * days);
-  } else if (vehicle === 2000) {
-    vehicle = 2000;
-    alert("El precio final del alquiler solicitado es $" + vehicle * days);
-  } else {
-    alert("Error!! Vuelva a intentarlo");
-  }
-  alert("Gracias por elegirnos!!");
+    if (electionVehicle === true) {
+      alert(
+        `Usted seleccionó un ${vehicle.id}. El precio por día de alquiler es de $${vehicle.price}`
+      );
+      return vehicle;
+    } else {
+      alert(`El vehículo seleccionado no existe. Intente nuevamente`);
+    }
+  } while (electionVehicle != true);
 }
-final(electionVehicles(), time());
-
 function time() {
-  let days = Number(
-    prompt("Seleccione la cantidad de días que desea alquilar el vehículo")
-  );
+  let days;
+  do {
+    days = Number(
+      prompt(`Seleccione la cantidad de días que desea alquilar el vehículo`)
+    );
+    if (!isNaN(days) && days >= 1 && Number.isInteger(days)) {
+      break;
+    } else {
+      alert(`Ingrese un dato válido`);
+    }
+  } while (isNaN(days) || days == 0 || days < 0 || !Number.isInteger(days));
   return days;
 }
-
-function electionVehicles() {
-  let price = 0;
-  let mapVehicles = vehicles.map((vehicle) => vehicle.id);
-  let electionVehicle = prompt(
-    `Hola!! ¿Qué tipo de vehículo desearías alquilar?\n${mapVehicles.join(
-      "\n"
-    )}`
-  ).toLowerCase();
-  switch (electionVehicle) {
-    case "moto":
-      alert(
-        `El vehículo seleccionado es una moto. El precio por día de alquiler es de $ 1000`
-      );
-      return (price = 1000);
-    case "auto":
-      alert(
-        `El vehículo seleccionado es un auto. El precio por día de alquiler es de $ 1500`
-      );
-      return (price = 1500);
-    case "camioneta":
-      alert(
-        `El vehículo seleccionado es una camioneta. El precio por día de alquiler es de $ 2000`
-      );
-      return (price = 2000);
-    default:
-      alert("El vehículo seleccionado no existe");
-      return price;
-  }
+function final(vehicle, price, time) {
+  alert(
+    `Usted seleccionó un ${vehicle} para alquilar por el plazo de ${time} días!! El precio final es de $${
+      price * time
+    }`
+  );
+  alert(`Gracias por elegirnos!!`);
 }
+electionVehicles();
+finalId = vehicle.id;
+finalPrice = vehicle.price;
+final(finalId, finalPrice, time());
